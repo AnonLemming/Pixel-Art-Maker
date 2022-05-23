ea52b09e4427e3d7faec7d61171aa0dd52089d5b
AnonLemming <106041045+AnonLemming@users.noreply.github.com>
2022-05-23 10:54:15 +0800

    Update designs.js
    First Iteration of makeGrid Function.

--- designs.js
+++ designs.js
@@ -4,7 +4,18 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
+  //Defining Row Variable
+  const drawGridRow = document.createElement('tr');
+  
+  //Drawing Grid Boxes
+  for (let i = 1; i <= (document.getElementById('inputHeight'); i++) {
+    const drawGridColumn = document.createElement('td');
+    for (let j = 1; j <= (document.getElementById('inputWidth'); j++) {
+      drawGridRow.appendChild(drawGridColumn);
+    }
+  }
+  //Attaching Grid Boxes to table
+  document.body.table.appendChild(drawGridRow);
 // Your code goes here!
 
 }
