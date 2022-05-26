28f078dc04f339ad04f402d801ad5870f0eb68d2
AnonLemming <106041045+AnonLemming@users.noreply.github.com>
2022-05-26 08:21:40 +0800

    design.js 0.0.6
    -Added variable declaration for table (#pixelCanvas)
    -Added drawGridRow call into parent for loop.
    -

--- designs.js
+++ designs.js
@@ -4,23 +4,25 @@ const selectColor = document.getElementById('colorPicker');
 const selectHeight = document.getElementById('inputHeight');
 const selectWidth = document.getElementById('inputWidth');
 const selectButton = document.querySelector('input[type=select]');
+const completeGrid = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 selectButton.addEventListener('click'; makeGrid);
 
 function makeGrid() {
-  //Defining Row Variable
+  //Defining Row Variable.
   const drawGridRow = document.createElement('tr');
   
-  //Drawing Grid Boxes
+  //Drawing Grid Boxes.
   for (let i = 1; i <= selectHeight; i++) {
+    drawGridRow;
     const drawGridColumn = document.createElement('td');
     for (let j = 1; j <= selectWidth; j++) {
       drawGridRow.appendChild(drawGridColumn);
     }
   }
-  //Attaching Grid Boxes to table
-  document.body.table.appendChild(drawGridRow);
-// Your code goes here!
+  
+  //Attaching Grid Boxes to completeGrid.
+  completeGrid.appendChild(drawGridRow);
 
 }
