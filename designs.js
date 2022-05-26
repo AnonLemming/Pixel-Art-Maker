7bfd304631fba8393a3a0d379d33233f56df95ec
AnonLemming <106041045+AnonLemming@users.noreply.github.com>
2022-05-26 12:56:08 +0800

    design.js 0.0.11
    -Added getAttribute call for height and width selectors.

--- designs.js
+++ designs.js
@@ -1,13 +1,13 @@
 // Select color input
 const selectColor = document.querySelector('#colorPicker');
 // Select size input
-const selectHeight = document.querySelector('#inputHeight');
-const selectWidth = document.querySelector('#inputWidth');
-const selectButton = document.querySelector('input[type="select"]');
-const completeGrid = document.querySelector('#pixelCanvas').innerHTML;
+const selectHeight = document.querySelector('#inputHeight').getAttribute('value');
+const selectWidth = document.querySelector('#inputWidth').getAttribute('value');
+const selectButton = document.querySelector('input[type="submit"]');
+const completeGrid = document.querySelector('table#pixelCanvas').innerHTML;
 
 // When size is submitted by the user, call makeGrid()
-selectButton.addEventListener('click'; makeGrid);
+selectButton.addEventListener('click', makeGrid);
 
 
 
@@ -20,12 +20,12 @@ function makeGrid() {
       const drawGridBox = document.createElement('td'); //Creating a Grid Box.
       drawGridRow.appendChild(drawGridBox); //Attaching Box to Row.
       console.log('box');
-      //drawGridBox.addEventListener('click'; paintPixel);
+      //drawGridBox.addEventListener('click'; paintPixel());
     }
   }
-  
+
   //Attaching Grid Boxes to completeGrid.
-  document.table.appendChild(drawGridRow);
+  completeGrid.appendChild(drawGridRow);
 }
 
 function paintPixel() {
