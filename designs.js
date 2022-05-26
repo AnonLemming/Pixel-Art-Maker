bae55c7f6505afbc3d9b6f8788239c75e79219fb
AnonLemming <106041045+AnonLemming@users.noreply.github.com>
2022-05-26 21:40:43 +0800

    desing.js 1.0.0
    Finalised functions
    Verified variables
    Completed comments
    Executed Events
    
    height and width can be successfully input to create a grid of any size.
    color can be selected from the color input
    clicking on a grid box will color that box with the selected color
    submitting the size again will refresh the grid

--- designs.js
+++ designs.js
@@ -1,35 +1,52 @@
-// Select color input
-let selectColor = document.querySelector('#colorPicker');
-// Select size input
-let selectHeight = document.querySelector('#inputHeight').getAttribute('value');
-let selectWidth = document.querySelector('#inputWidth').getAttribute('value');
-const selectButton = document.querySelector('input[type="submit"]');
-var completeGrid = document.querySelector('table#pixelCanvas').innerHTML;
+// initial variable declarations
+const color = document.querySelector('#colorPicker');  //variable for color input
+const height = document.querySelector('#inputHeight');  //variable for height input
+const width = document.querySelector('#inputWidth');  //variable for width input
+const sizeSelector = document.querySelector('#sizePicker');  //constant for size picker form
+const completeGrid = document.querySelector('#pixelCanvas');  //variable for table/grid/canvas
+let selectedHeight = height.value;  //variable for height value.
+let selectedWidth = width.value;  //variable for width value.
+let selectedColor = color.value;  //variable for color value.
 
-// When size is submitted by the user, call makeGrid()
-selectButton.addEventListener('click', makeGrid);
+//event to change the value of the height.
+height.addEventListener('change', function() {
+  selectedHeight = height.value;
+});
 
+//event to change the value of the width.
+width.addEventListener('change', function() {
+  selectedWidth = width.value;
+});
 
+//event to change the value of the color.
+color.addEventListener('change', function() {
+  selectedColor = color.value;
+})
 
-function makeGrid() {
-  //Drawing Grid Boxes.
-  for (let i = 1; i <= selectHeight; i++) {
-    const drawGridRow = document.createElement('tr'); //Creating a Grid Row.
-    drawGridRow;
-    //console.log('row');
-    for (let j = 1; j <= selectWidth; j++) {
-      const drawGridBox = document.createElement('td'); //Creating a Grid Box.
-      drawGridBox;
-      //drawGridBox.addEventListener('click'; paintPixel);
-      drawGridRow.appendChild(drawGridBox); //Attaching Boxes to Row.
-      console.log('box');
-    }
+// When size is submitted by the user, calls makeGrid()
+sizeSelector.addEventListener('submit', function(evt){
+  evt.preventDefault();  //prevents clearing other input values
+  while (completeGrid.firstChild) {  //clears previously created child elements
+    completeGrid.removeChild(completeGrid.firstChild);
   }
+  makeGrid();  //calls function to create grid/canvas.
+});
 
-  //Attaching Grid Boxes to completeGrid.
-  completeGrid.appendChild(drawGridRow);
+//Drawing Grid Boxes.
+function makeGrid() {
+  for (let i = 1; i <= selectedHeight; i++) {
+    const drawGridRow = document.createElement('tr');  //Creating a Grid Row.
+
+    for (let j = 1; j <= selectedWidth; j++) {
+      const drawGridBox = document.createElement('td');  //Creating a Grid Box.
+      drawGridBox.addEventListener('click', paintPixel);  //attached event for coloring pixel/Box.
+      drawGridRow.appendChild(drawGridBox);  //Attaching Boxes to Row.
+    }
+    completeGrid.appendChild(drawGridRow);  //Attaching Rows to completeGrid.
+  }
 }
 
-function paintPixel() {
-  //
+//function to color the "pixel/Box" <td> elements
+function paintPixel(e) {
+  this.style.backgroundColor = selectedColor;
 }
